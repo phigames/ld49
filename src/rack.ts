07b456414ce0f9179bfb075a376a858f708321da
ThThelitz <thilo.thelitz@gmail.com>
2021-10-02 16:01:41 +0200

    Basic rack

--- src/rack.ts
+++ src/rack.ts
@@ -2,16 +2,19 @@ import Tile from "./tile";
 
 export default class Rack extends Phaser.GameObjects.Container {
   tiles: Tile[];
+  letters: string[];
 
-  constructor(scene: Phaser.Scene, letters: string[]) {
+  constructor(scene: Phaser.Scene) {
     super(scene, 400, 500);
 
     this.tiles = [];
-    for (const value of letters) {
-      const tile = new Tile(scene, value, 0, 0);
+    this.letters = ["C", "L", "M", "A", "C", "H", "T", "S"];
+    let next_tile_x = -146;
+    for (const value of this.letters) {
+      const tile = new Tile(scene, value, next_tile_x, 0);
       this.tiles.push(tile);
       this.add(tile);
+      next_tile_x = next_tile_x + 36;
     }
   }
 }
-
