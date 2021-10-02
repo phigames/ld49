5e2a5de9d3db632004e97ebceb968329ed2df4c6
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-10-02 18:34:22 +0200

    [wip] make column tiles draggable

--- src/column.ts
+++ src/column.ts
@@ -1,8 +1,12 @@
 import { Dictionary } from "./dictionary";
+import * as C from "./constants";
 import Tile from "./tile";
 
 export default class Column extends Phaser.GameObjects.Container {
   tiles: Tile[];
+  draggingTile: Tile | null;
+  dragStartX: number;
+  dragStartY: number;
   dictionary: Dictionary;
   isWord: boolean;
 
@@ -15,17 +19,34 @@ export default class Column extends Phaser.GameObjects.Container {
   ) {
     super(scene, index * 70, 20);
     this.tiles = [];
+    this.draggingTile = null;
     this.isWord = false
     this.dictionary = dictionary;
     letters.forEach((l, i) => {
-      let tile = new Tile(scene, l, index * 70, i * 40)
-      this.addTile(tile)
+      let tile = new Tile(scene, l, index * 70, i * 40);
+      this.addTile(tile);
     });
-    
+
     this.on("pointerup", callback);
   }
 
-  addTile(tile: Tile){
+  addTile(tile: Tile) {
+    tile.removeAllListeners();
+    tile.setInteractive();
+    this.scene.input.setDraggable(tile);
+    tile.on("dragstart", (pointer: Phaser.Input.Pointer) => {
+      this.draggingTile = tile;
+    });
+    tile.on(
+      "drag",
+      (pointer: Phaser.Input.Pointer, dragX: number, dragY: number) => {
+        tile.x = dragX;
+        tile.y = dragY;
+      }
+    );
+    tile.on("dragend", (pointer: Phaser.Input.Pointer) => {
+      this.draggingTile = null;
+    });
     this.tiles.push(tile);
     this.add(tile)
     this.checkCorrectWord()
@@ -36,7 +57,7 @@ export default class Column extends Phaser.GameObjects.Container {
     this.tiles.splice(index, 1)
     this.checkCorrectWord()
   }
-  
+
   addNewButton() {
     this.add(
       new Phaser.GameObjects.Image(this.scene, this.x, this.y + 120, `letter-X`)
