c73ccba0ea93eb3e9fe41a5e44e227a0be51ced4
ThThelitz <thilo.thelitz@gmail.com>
2021-10-02 15:37:46 +0200

    Give tiles x and y, give rack coordinates

--- src/rack.ts
+++ src/rack.ts
@@ -4,10 +4,11 @@ export default class Rack extends Phaser.GameObjects.Container {
   tiles: Tile[];
 
   constructor(scene: Phaser.Scene, letters: string[]) {
-    super(scene);
+    super(scene, 400, 500);
+
     this.tiles = [];
     for (const value of letters) {
-      const tile = new Tile(scene, value);
+      const tile = new Tile(scene, value, 0, 0);
       this.tiles.push(tile);
       this.add(tile);
     }
--- src/tile.ts
+++ src/tile.ts
@@ -1,8 +1,8 @@
 export default class Tile extends Phaser.GameObjects.Image {
   letter: string;
 
-  constructor(scene: Phaser.Scene, letter: string) {
-    super(scene, 50, 50, `letter-${letter}`);
+  constructor(scene: Phaser.Scene, letter: string, x: integer, y: integer) {
+    super(scene, x, y, `letter-${letter}`);
     this.letter = letter;
   }
 }
