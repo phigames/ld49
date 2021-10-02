78a57bee408b60f1a68ad2f83db23e3b23fad212
veralara.bernhard <veralara.bernhard@uzh.ch>
2021-10-02 17:33:01 +0200

    Add function to column button

--- src/column.ts
+++ src/column.ts
@@ -4,41 +4,52 @@ import Tile from "./tile";
 export default class Column extends Phaser.GameObjects.Container {
   tiles: Tile[];
   dictionary: Dictionary;
+  button: Phaser.GameObjects.Image | null;
+  buttonAddsTile: Function | null;
 
   constructor(
     scene: Phaser.Scene,
     index: number,
     letters: string[],
     dictionary: Dictionary,
-    callback
+    buttonAddsTile
   ) {
     super(scene, index * 70, 20);
     this.tiles = [];
     this.dictionary = dictionary;
     letters.forEach((l, i) => {
-      let tile = new Tile(scene, l, index * 70, i * 40)
-      this.addTile(tile)
+      let tile = new Tile(scene, l, index * 70, i * 40);
+      this.addTile(tile);
     });
-    
-    this.on("pointerup", callback);
+    this.button = null;
+    this.buttonAddsTile = buttonAddsTile;
   }
 
-  addTile(tile: Tile){
+  addTile(tile: Tile) {
     this.tiles.push(tile);
-    this.add(tile)
+    this.add(tile);
   }
 
-  removeTile(index: number){
-    this.tiles.splice(index, 1)
+  removeTile(index: number) {
+    this.tiles.splice(index, 1);
   }
-  
+
   addNewButton() {
-    this.add(
-      new Phaser.GameObjects.Image(this.scene, this.x, this.y + 120, `letter-X`)
+    let button = new Phaser.GameObjects.Image(
+      this.scene,
+      this.x,
+      this.y + 120,
+      `letter-X`
     );
+    this.button = button;
+    this.add(button);
+    this.button.setInteractive();
+    this.button.on("pointerup", this.buttonAddsTile);
   }
 
-  removeButton() {}
+  removeButton() {
+    this.button = null;
+  }
 
   getWordString() {
     let word = "";
