83afbe5ddcba5edae391b65b4a4052ecc476f6b3
veralara.bernhard <veralara.bernhard@uzh.ch>
2021-10-02 16:27:07 +0200

    Update columns

--- src/column.ts
+++ src/column.ts
@@ -1,18 +1,21 @@
 import Tile from "./tile";
 
 export default class Column extends Phaser.GameObjects.Container {
-  letters: Tile[];
+  tiles: Tile[];
 
-  constructor(scene: Phaser.Scene, index: number, letters: string[]) {
+  constructor(scene: Phaser.Scene, index: number, letters: string[], callback) {
     super(scene, index * 70, 20);
-    this.letters = [];
+    this.tiles = [];
     letters.forEach((l, i) => {
-      this.letters.push(new Tile(scene, l, index * 70, i * 40));
+      this.tiles.push(new Tile(scene, l, index * 70, i * 40));
     });
-    for (let tile of this.letters){
+    for (let tile of this.tiles){
       this.add(tile)
     }
+    this.on('pointerup', callback)
   }
+
+
 
   addNewButton() {
     this.add(new Phaser.GameObjects.Image(this.scene, this.x , this.y + 120, `letter-X`));
