9330352132b8f4a2bc819e547e5aacfa9c8a8166
ThThelitz <thilo.thelitz@gmail.com>
2021-10-03 08:44:06 +0200

    Add dummy "rackable" property

--- src/tile.ts
+++ src/tile.ts
@@ -1,9 +1,11 @@
 export default class Tile extends Phaser.GameObjects.Image {
   letter: string;
+  rackable: boolean;
 
   constructor(scene: Phaser.Scene, letter: string, x: integer, y: integer) {
     super(scene, x, y, `letter-${letter}`);
     this.letter = letter;
     this.setInteractive();
+    this.rackable = true;
   }
 }
